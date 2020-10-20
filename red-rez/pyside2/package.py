95530fa88c99b55387d145fec2b57c426aff5292
DanieleBerna <45497530+DanieleBerna@users.noreply.github.com>
2020-10-20 10:21:13 +0200

    Removed not needed files

--- red-rez/pyside2/package.py
+++
@@ -1,8 +0,0 @@
-name = 'pyside2'
-version = '5.15.0'
-requires=["python"]
-build_command = "py {root}/build.py "+version 
-
-def commands():
-	global env
-	env['PYTHONPATH'].prepend('{root}')
--- red-rez/test_resolve.bat
+++
@@ -1,2 +0,0 @@
-IF "%REZ_CONFIG_FILE%"=="" SET REZ_CONFIG_FILE=T:\ugcore\rez\rezconfig.py
-T:\ugcore\rez\Scripts\rez\rez-env python PySide2 -- python T:\minimal_pyside2.py
