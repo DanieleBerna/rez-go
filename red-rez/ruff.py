e399a33b2a8a6cb0077f001866827ff7e387a08e
DanieleBerna <bdcreations@gmail.com>
2020-10-29 23:09:43 +0100

    arguments added in subparser with a for loop

--- red-rez/ruff.py
+++ red-rez/ruff.py
@@ -6,22 +6,15 @@ def parse_arguments():
 
     subparsers = parser.add_subparsers(help='Modes', dest='mode', required=True)
     parser_install = subparsers.add_parser('install', help='Create a new rez setup in the local folder')
-
-    parser_install.add_argument("-m", "--map", action="store", type=str, dest="unit",
-                        help="Map the local folder to another disk unit during the install process")
-
-    parser_install.add_argument("-r", "--release", action="store", type=str, dest="release_folder",
-                        help="Set a remote folder as release_packages_path")
-
     parser_pack = subparsers.add_parser('pack', help='Pack the existing rez given the local folder in a zip file')
-
     parser_deploy = subparsers.add_parser('deploy', help='Unpack and deploy to the local folder a previously zipped rez')
 
-    parser_deploy.add_argument("-m", "--map", action="store", type=str, dest="unit",
-                                help="Map the local folder to another disk unit during the install process")
+    for p in (parser_install, parser_deploy):
+        p.add_argument("-m", "--map", action="store", type=str, dest="unit",
+                        help="Map the local folder to another disk unit during the install process")
 
-    parser_deploy.add_argument("-r", "--release", action="store", type=str, dest="release_folder",
-                                help="Set a remote folder as release_packages_path")
+        p.add_argument("-r", "--release", action="store", type=str, dest="release_folder",
+                        help="Set a remote folder as release_packages_path")
 
     parser.add_argument("local_folder", type=str,
                           help="rez local folder")
