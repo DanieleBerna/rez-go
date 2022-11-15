14e8a3202f254c1db4be21a42e2f180b85d3fbf3
DanieleBerna <45497530+DanieleBerna@users.noreply.github.com>
2022-11-15 11:40:31 +0100

    added extension to blender in create exe batch file

--- rez-go/create-exe.bat
+++ rez-go/create-exe.bat
@@ -4,12 +4,19 @@ copy .\dist\rezgo.exe .\
 @echo off
 (
   echo set /p "installfolder=Enter installation folder: "
-  echo .\rezgo.exe install %%installfolder%% -d
+  echo .\rezgo.exe install %%installfolder%% -d -p
+  echo copy .\blender_city20.bat %%installfolder%%\utgtools\core\launchers\blender_city20.bat
+  echo copy .\blender.bat %%installfolder%%\utgtools\core\launchers\blender.bat
 ) > setup.bat
 
-tar.exe acvf rezgo.zip payload rezgo.exe setup.bat
+echo rez-env blender-3.3 city20_texturingtool blender_ug_exporter -- blender.exe --app-template city20 --python-use-system-env --python %%%%REZ_BLENDER_BOOTSCRIPT%%%% > blender_city20.bat
+echo rez-env blender-3.3 -- blender.exe --python-use-system-env --python %%%%REZ_BLENDER_BOOTSCRIPT%%%% > blender.bat
+
+tar.exe acvf rezgo.zip payload rezgo.exe setup.bat blender_city20.bat blender.bat
 del .\rezgo.exe
 del .\setup.bat
+del .\blender_city20.bat
+del .\blender.bat
 
 
 
